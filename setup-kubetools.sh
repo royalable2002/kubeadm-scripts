a36caa22dac0b5a1b44b9e29f76bc62cb5147f4f
Milad Tousi <34961472+royalable2002@users.noreply.github.com>
2022-06-28 12:22:39 +0430

    Update setup-kubetools.sh

--- setup-kubetools.sh
+++ setup-kubetools.sh
@@ -24,8 +24,7 @@ then
 name=Kubernetes
 baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
 enabled=1
-gpgcheck=1
-repo_gpgcheck=1
+
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 exclude=kubelet kubeadm kubectl
 EOF
